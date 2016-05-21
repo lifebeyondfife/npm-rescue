79586fa9ee45ff33188eb6b3b7fa9067868d37bb
Iain McDonald <iain@lifebeyondfife.com>
2016-05-21 16:14:44 +0100

    More debugging needed to resolve synchronous behaviour.

--- rescue.js
+++ rescue.js
@@ -36,34 +36,54 @@ const repoConfig = loadConfig.then(config => {
         const npmRescuePackage = path.resolve(config.gitDirectory, 'package.json');
 
         let index = undefined;
-        let parent = undefined;
-        let currentPromise = new Promise((resolve, reject) => { resolve(); });
+
+/*
+
+Aha, there is asynchronous weirdness going on that needs to be fixed.
+
+Wrote /home/iain/dev/flux/package.json to /home/iain/dev/npm-rescue/npm-rescue-backup/package.json.
+Wrote /home/iain/dev/flux-todomvc/package.json to /home/iain/dev/npm-rescue/npm-rescue-backup/package.json.
+Wrote /home/iain/dev/npm-rescue/package.json to /home/iain/dev/npm-rescue/npm-rescue-backup/package.json.
+Wrote /home/iain/dev/todo-redux-react-webpack/package.json to /home/iain/dev/npm-rescue/npm-rescue-backup/package.json.
+Wrote /home/iain/dev/react-tutorial/package.json to /home/iain/dev/npm-rescue/npm-rescue-backup/package.json.
+New commit for flux.git: 0000000000000000605d39030000000020000000.
+Checkpoint
+New commit for flux-todomvc.git: 709839030000000040e733030000000000000000.
+Checkpoint
+New commit for npm-rescue.git: 50ca39030000000020e433030000000000000000.
+Checkpoint
+New commit for todo-redux-react-webpack.git: 205b390300000000e0f739030000000000000000.
+Checkpoint
+New commit for react-tutorial.git: a0f539030000000080e833030000000000000000.
+Checkpoint
+
+*/
 
         config.npmPackages.forEach(npmPackage => {
-            var nextPromise = currentPromise.then(() => {
-                return git.Branch.lookup(repo, npmPackage.projectName, git.Branch.BRANCH.LOCAL);
-            }).then(branch => {
-                parent = branch.target();
-                repo.checkoutBranch(branch);
-            }).then(() => {
-                fs.copySync(npmPackage.npmPackage, npmRescuePackage);
-                return repo.index();
-            }).then(i => {
-                index = i;
-                return index.addAll(git.Pathspec.create(['.']), git.Index.ADD_OPTION.ADD_DEFAULT);
-            }).then(() => {
-                return index.write();
-            }).then(() => {
-                return index.writeTree();
-            }).then(tree => {
-                return repo.createCommit('HEAD', signature, signature, `npm rescue backup of ${npmPackage.projectName}.`, tree, [parent]);
-            }).then(oid => {
-                console.log(`New commit for ${npmPackage.projectName}: ${oid.toString()}.`);
-            }).catch(error => {
-                console.log(error.message);
-            });
-
-            currentPromise = nextPromise;
+            Promise.all([
+                git.Branch.lookup(repo, npmPackage.projectName, git.Branch.BRANCH.LOCAL).then(branch => {
+                    repo.checkoutBranch(branch);
+                }).then(() => {
+                    fs.copySync(npmPackage.npmPackage, npmRescuePackage);
+                    console.log(`Wrote ${npmPackage.npmPackage} to ${npmRescuePackage}.`);
+                    return repo.index();
+                }).then(i => {
+                    index = i;
+                    return index.addAll(git.Pathspec.create(['.']), git.Index.ADD_OPTION.ADD_DEFAULT);
+                }).then(() => {
+                    return index.write();
+                }).then(() => {
+                    return index.writeTree();
+                }).then(tree => {
+                    return repo.getHeadCommit().then(parent => {
+                        return repo.createCommit('HEAD', signature, signature, `npm rescue backup of ${npmPackage.projectName}.`, tree, [parent]);
+                    });
+                }).then(oid => {
+                    console.log(`New commit for ${npmPackage.projectName}: ${oid.toString()}.`);
+                }).catch(error => {
+                    console.log(error.message);
+                })
+            ]).then(s => console.log('Checkpoint'));
         });
     }).catch(error => {
         console.log(error.message);
