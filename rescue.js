a93bcb881981137c13d78e0a836803881dd3e986
Iain McDonald <iain@lifebeyondfife.com>
2016-05-30 21:14:46 +0100

    Almost finished. Just need to get spawn respect  the cwd.

--- rescue.js
+++ rescue.js
@@ -1,25 +1,20 @@
-/*
- *  1.  Check npm-rescue-config.json exists
- *  2.  Parse JSON, set variables for the git directory and npmPackages
- *  3.  Create nodegit repo object for git directory (promise)
- *  4.  (wait for 3. then) Enumerate npmPackages
- *  4.1.    git checkout <npmPackage.projectName>
- *  4.2.    cp package.json to <gitDirectory> from <npmPackage.npmPackage>
- *  4.3.    exec("npm install")
-    4.4.    git add .
-    4.5.    git commit -m "Npm Rescue Backup at <XYZ> o'clock"
- */
 'use strict';
 const fs = require('fs-extra');
 const git = require('simple-git');
 const path = require('path');
+const spawn = require('child_process').spawn;
+const moment = require('moment');
 const loadConfig = require('./src/loadConfig');
+
+const printLine = line => {
+    console.log(`${line}`);
+};
 
 const repoConfig = loadConfig.then(config => {
     const repo = git(config.gitDirectory);
     const npmRescuePackage = path.resolve(config.gitDirectory, 'package.json');
 
-    var processPromise = npmPackages => {
+    var backupPackages = npmPackages => {
         var npmPackage = npmPackages.pop();
 
         if (npmPackage == undefined) {
@@ -29,38 +24,32 @@ const repoConfig = loadConfig.then(config => {
         return new Promise((resolve, reject) => {
             repo.checkout(npmPackage.projectName).then(() => {
                 fs.copySync(npmPackage.npmPackage, npmRescuePackage);
-                //  Put 4.3 here.
-                return resolve(repo.
-                    add(['package.json']).
-                    commit(`npm rescue backup of ${npmPackage.projectName}.`)
-                );
+
+                const npmInstall = spawn('npm', ['install'], { cwd: path.dirname(npmPackage.npmPackage) });
+                npmInstall.stdout.on('data', printLine);
+                npmInstall.stderr.on('data', printLine);
+
+                npmInstall.on('close', code => {
+                    console.log(`child process exited with code ${code}`);
+
+                    return resolve(repo.
+                        add('./*').
+                        commit(`npm rescue backup of ${npmPackage.projectName} at ${moment().format('D MMM HH:MM')}.`)
+                    );
+                });
             });
         }).then(() => {
             console.log(`Backing up ${npmPackage.projectName}...`);
-            return processPromise(npmPackages);
+            return backupPackages(npmPackages);
         });
     };
 
-    processPromise(config.npmPackages).then(() => {
+    //  Backup action is chained recursively with promises
+    //  to keep the order deterministic while doing file I/O
+    backupPackages(config.npmPackages).then(() => {
         console.log('Finished.');
     });
 }).catch(error => {
     console.log(error.message);
     process.exit(1);
 });
-
-/*  4.3
-const spawn = require('child_process').spawn;
-const npmInstall = spawn('npm', ['install']);
-
-const printLine = line => {
-    console.log(`${line}`);
-};
-
-npmInstall.stdout.on('data', printLine);
-npmInstall.stderr.on('data', printLine);
-
-npmInstall.on('close', (code) => {
-    console.log(`child process exited with code ${code}`);
-});
-*/
