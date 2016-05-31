20063d7e9f748e4074bc09d83cba138abd50ef74
Iain McDonald <iain@lifebeyondfife.com>
2016-05-31 22:02:25 +0100

    Restructure project to make execution simpler.

---
+++ src/initialise.js
@@ -0,0 +1,64 @@
+'use strict';
+const fs = require('fs');
+const path = require('path');
+const findNpmPackages = require('./findNpmPackages');
+const getRepoNames = require('./getRepoNames');
+const userPrompt = require('./userPrompt');
+const createRepo = require('./createRepo');
+const headCommit = require('./headCommit');
+const createBranches = require('./createBranches');
+
+const initialise = npmRescueConfig => {
+    return userPrompt().then(userProperties => {
+        const searchPath = userProperties[0];
+        const gitInitPath = userProperties[1];
+
+        let gitDirectory = undefined;
+        let npmPackages = undefined;
+        let repo = undefined;
+        let branchOid = undefined;
+
+        return Promise.all([findNpmPackages(searchPath).then(packages => {
+                if (!packages.length) {
+                    console.log(`Cannot find any git projects with top level npm packages in ${directory}`)
+                } else {
+                    console.log(`Found ${packages.length} npm package(s)`)
+                }
+
+                return getRepoNames(packages);
+            }),
+            createRepo(gitInitPath).then(git => {
+                gitDirectory = git.gitDirectory;
+                return headCommit(git.repo, git.gitDirectory);
+            })
+        ]).then(values => {
+            npmPackages = values[0];
+            repo = values[1];
+
+            return createBranches(npmPackages, repo);
+        }).then(() => {
+            const config = JSON.stringify({
+                    npmPackages: npmPackages,
+                    gitDirectory: path.resolve(gitDirectory),
+                },
+                null, 4
+            );
+
+            return new Promise((resolve, reject) => {
+                fs.writeFile(npmRescueConfig, config, error => {
+                    if (error) {
+                        console.log(`Error creating ${npmRescueConfig}`);
+                        process.exit(1);
+                    }
+
+                    console.log(`Created config file in ${npmRescueConfig}...`);
+                    resolve();
+                });
+            });
+        }).catch(error => {
+            console.log(error.message);
+        });
+    });
+};
+
+module.exports = initialise;
--- src/loadConfig.js
+++ src/loadConfig.js
@@ -1,27 +1,29 @@
 'use strict';
 const fs = require('fs');
 
-const loadConfig = new Promise((resolve, reject) => {
-    fs.readFile('./npm-rescue-config.json', (error, data) => {
-        if (error) {
-            return reject(Error('Cannot find npm-rescue-config.json. Please run "nodejs initialise.js".'));
-        }
-
-        try {
-            const parsedJson = JSON.parse(data);
-
-            if (!parsedJson.gitDirectory || !parsedJson.npmPackages) {
-                return reject(Error('Cannot find gitDirectory of npmPackages in npm-rescue-config.json. Please run "nodejs initialise.js".'));
+const loadConfig = npmRescueConfig => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(npmRescueConfig, (error, data) => {
+            if (error) {
+                return reject(Error('Cannot find npm-rescue-config.json. Please run "nodejs initialise.js".'));
             }
 
-            return resolve(parsedJson);
-        } catch (error) {
-            return reject(error);
-        }
+            try {
+                const parsedJson = JSON.parse(data);
+
+                if (!parsedJson.gitDirectory || !parsedJson.npmPackages) {
+                    return reject(Error('Cannot find gitDirectory of npmPackages in npm-rescue-config.json. Please run "nodejs initialise.js".'));
+                }
+
+                return resolve(parsedJson);
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }).catch(error => {
+        console.log(error.message);
+        process.exit(1);
     });
-}).catch(error => {
-    console.log(error.message);
-    process.exit(1);
-});
+};
 
 module.exports = loadConfig;
